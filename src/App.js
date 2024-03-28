71c4e5f38050e93acd583b23538a79ca1d7f1ea5
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2024-03-28 11:55:00 +0500

    ibrohim

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
